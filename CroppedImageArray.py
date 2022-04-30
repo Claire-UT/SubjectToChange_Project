f51de4538e362d021307fd09145ca48016001a58
DavidSpitler23 <98062563+DavidSpitler23@users.noreply.github.com>
2022-04-29 23:18:00 -0500

    Update CroppedImageArray.py

--- CroppedImageArray.py
+++ CroppedImageArray.py
@@ -23,16 +23,18 @@ while not good:
         good = True
 
 img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-ret, im = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
+ret, im = cv2.threshold(img_gray, 70, 255, cv2.THRESH_BINARY_INV)
 contours, hierarchy  = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+thresh = cv2.threshold(img_gray, 70, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+img_dilate = cv2.dilate(thresh, kernel, iterations=1)
 
 blank = np.zeros(image.shape, dtype='uint8')
 new_img = cv2.drawContours(blank,contours, -1, (0,0,0))
 cropped_images = []
 predictions = []
-resized = []
+#resized = []
 
 # dimension of image for machine learning:
 dim = (28,28)
@@ -41,22 +43,25 @@ for contour in contours:
     (x,y,w,h) = cv2.boundingRect(contour)
     # for now, try just making it square by using the max of width, height
     w,h = max(w,h), max(w,h)
-    crp = im[y:y+h,x:x+w]
+    #crp = im[y:y+h,x:x+w]
     #resize to correct dimensions
     resized_image = cv2.resize(thresh, dim,interpolation = cv2.INTER_AREA)
-    cropped_images.append((im[y:y+h,x:x+w],[x,y,w,h]))
-    resized.append(resized_image)
+    #cropped_images.append((im[y:y+h,x:x+w],[x,y,w,h]))
+    #resized.append(resized_image)
 
 cropped = thresh[y:y+h,x:x+w]
 resized_cropped = cv2.resize(cropped, dim,interpolation = cv2.INTER_AREA)
+Big = cv2.resize(cropped, (280,280),interpolation = cv2.INTER_AREA)
 # print(cropped.astype('int'))
 # show_image(cropped)
 
 # print(resized_image.astype('int'))
 # show_image(resized_image)
+show_image(thresh)
+show_image(img_dilate)
+show_image(Big)
 
 print(resized_cropped.astype('int'))
 show_image(resized_cropped)
-
 
 
