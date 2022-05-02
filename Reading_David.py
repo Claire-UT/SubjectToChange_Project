173c462249a5ea3c55b6bc5bb57dd941147e8534
Claire-UT <97992432+Claire-UT@users.noreply.github.com>
2022-05-01 23:01:56 -0500

    Update Reading_David.py

--- Reading_David.py
+++ Reading_David.py
@@ -6,95 +6,112 @@ from tensorflow import keras
 import tensorflow as tf
 
 # Import required packages
- 
-# Read image from which text needs to be extracted
-img = cv2.imread("DavidTest6.jpg")
-cv2.imshow('Original', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# Preprocessing the image starts
+import cv2
+import numpy as np
 
-#Smooth the image
-kernel = np.ones((5,5),np.float32)/25
-smooth = cv2.filter2D(img,-1,kernel)
-# cv2.imshow('Smooth', smooth)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
- 
-# Convert the image to gray scale
-gray = cv2.cvtColor(smooth, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('Gray', gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
- 
-# Performing OTSU threshold
-ret, thresh1 = cv2.threshold(gray, 100, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-# cv2.imshow('thresh1',thresh1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
- 
-# Specify structure shape and kernel size.
-# Kernel size increases or decreases the area
-# of the rectangle to be detected.
-# A smaller value like (10, 10) will detect
-# each word instead of a sentence.
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
- 
-# Applying dilation on the threshold image
-dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
-# cv2.imshow('Dilation',dilation)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# Finding contours
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-                                                 cv2.CHAIN_APPROX_NONE)
- 
-# Creating a copy of image
-im2 = img.copy()
-
-#Test Data File Dimensions to be resized to
-dim = (28,28)
-  
-# Looping through the identified contours
-# Then rectangular part is cropped and passed on
-# to pytesseract for extracting text from it
-# Extracted text is then written into the text file
-alltext=[]
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
-    
-    if w < (h):
-        x = int(x - ((h-w)/2))
-        
-    #Make it square by using the max of width, height
-    w,h = max(w,h), max(w,h)
-    
-    #x = int(x - (0.1*x))
-    
-    #cropped = thresh1[y:y+ int(h*.9),x:x+ int(w*.9)]
-    # invthresh = cv2.bitwise_not(thresh1)
-    invthresh = thresh1
-    cropped = invthresh[y:y+h,x:x+w]
-    # cv2.imshow('Pre-re dim',cropped)
+def show_image(image):
+    cv2.imshow('image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    resized_cropped = cv2.resize(cropped, dim, interpolation = cv2.INTER_AREA)
-    Big = cv2.resize(cropped, (280,280),interpolation = cv2.INTER_AREA)
+
+cam = cv2.VideoCapture(0)
+
+while True:
+
+    #Show the video feed
+    result, img = cam.read()
+    cv2.imshow('LiveStream', img)    
+
+    # Preprocessing the image starts
     
-    # # Drawing a rectangle on copied image
-    # rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #Smooth the image
+    kernel = np.ones((5,5),np.float32)/25
+    smooth1 = cv2.filter2D(img,-1,kernel)
+    smooth = cv2.filter2D(smooth1,-1,kernel)
      
-    # # Cropping the text block for giving input to OCR
-    # cropped = im2[y:y + h, x:x + w]
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(smooth, cv2.COLOR_BGR2GRAY)
+     
+    # Performing OTSU threshold
+    ret, thresh1 = cv2.threshold(gray, 100, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    #thresh1 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
     
-    cv2.imshow('Cropped', Big)
-    # print(resized_cropped.astype('int'))
-    alltext.append(resized_cropped)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #Blur threshold to get rid of any dots
+    blur = cv2.medianBlur(thresh1, 3)
+     
+    # Specify structure shape and kernel size.
+    # Kernel size increases or decreases the area
+    # of the rectangle to be detected.
+    # A smaller value like (10, 10) will detect
+    # each word instead of a sentence.
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
+     
+    # Applying dilation on the threshold image
+    dilation = cv2.dilate(blur, rect_kernel, iterations = 1)
+    dilation2 = cv2.dilate(blur, rect_kernel, iterations = 3)
+    
+    # Finding contours
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                                     cv2.CHAIN_APPROX_NONE)
+     
+    # Creating a copy of image
+    im2 = img.copy()
+    
+    #Test Data File Dimensions to be resized to
+    dim = (28,28)
+      
+    # Looping through the identified contours
+    # Then rectangular part is cropped and padded with white space to make square
+    # This is resized to 28x28 and also shown in 280x280
+    alltext=[]
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        
+        old_image_height = h
+        old_image_width = w
+        new_image_width = max(w,h)
+        new_image_height = max(w,h)
+        mask = np.full((new_image_height,new_image_width), 255, dtype=np.uint8)
+        
+        # compute center offset
+        x_center = (new_image_width - old_image_width) // 2
+        y_center = (new_image_height - old_image_height) // 2
+        
+        invthresh = cv2.bitwise_not(thresh1)
+        cropped = invthresh[y:y+h,x:x+w]
+    
+        # copy img image into center of result image
+        mask[y_center:y_center+old_image_height,x_center:x_center+old_image_width] = cropped
+        
+        #Resize square, padded value to input dim and larger viewable 280x280 size
+        resized_cropped = cv2.resize(mask, dim, interpolation = cv2.INTER_AREA)
+        Big = cv2.resize(mask, (280,280),interpolation = cv2.INTER_AREA)
+        resized_cropped=np.array(resized_cropped)
+        alltext.append(resized_cropped)
+        
+        # print(resized_cropped.astype('int'))
+        #cv2.imshow('28x28 Image', resized_cropped)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+    # cv2.imshow('280x280 Image', Big)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+alltext=np.array(alltext)
+print(alltext)
+cam.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
 
 resized_cropped=alltext
+
+#ensures that the model receives a 3D array. This would be 2D if there is only one character scanned
+# print(resized_cropped.ndim)
+if resized_cropped.ndim < 3:
+    resized_cropped=[resized_cropped]
 # EMNIST PREDICTIONS ----------------------------------------
 mapping = {    
     # digits
@@ -115,8 +132,8 @@ mapping = {
 model = keras.models.load_model('EMNIST Model')
 # print(resized_cropped)
 resized_cropped=np.array(resized_cropped)
-print(resized_cropped[0])
-print(type(resized_cropped[0][0][0]))
+# print(resized_cropped[0])
+# print(type(resized_cropped[0][0][0]))
 resized_cropped = resized_cropped.astype(float)
 # resized_cropped = resized_cropped / 255.0
 for image in resized_cropped:
@@ -125,8 +142,8 @@ for image in resized_cropped:
             row[i] = row[i] / 255.00
             # print(type(row[i]))
             
-print(resized_cropped[0])
-print(type(resized_cropped[0][0][0]))
+# print(resized_cropped[0])
+# print(type(resized_cropped[0][0][0]))
 # !IMPORTANT! below line arbitrarily adds the image into an ndarray of images, since this is the input the model accepts. Assumed that in the future, resized will already contain multiple images (3d ndarray)
 
 resized_cropped = np.array(resized_cropped)
@@ -138,10 +155,10 @@ numberImages=np.size(resized_cropped, 0)
 # print("numberImages: " + str(numberImages))
 train_images_height = 28
 train_images_width = 28
-print(f"Resized Reshape: {resized_cropped.shape}")
+# print(f"Resized Reshape: {resized_cropped.shape}")
 resized_cropped= resized_cropped.reshape(numberImages, train_images_height, train_images_width, 1)
 # print(resized_cropped[0])
-print(f"Resized Reshape: {resized_cropped.shape}")
+# print(f"Resized Reshape: {resized_cropped.shape}")
 
 predictions=model.predict(resized_cropped)
 # print('done')
